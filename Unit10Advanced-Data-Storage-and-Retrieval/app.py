cf0741911492dfe116b26401ab6d397c81a3304a
hsnguyen1995 <53453390+hsnguyen1995@users.noreply.github.com>
2019-11-17 01:41:45 -0800

    update app.py

--- Unit10Advanced-Data-Storage-and-Retrieval/app.py
+++ Unit10Advanced-Data-Storage-and-Retrieval/app.py
@@ -1,3 +1,4 @@
+# Dependencies
 import numpy as np
 import pandas as pd
 import datetime as dt
@@ -30,7 +31,6 @@ app = Flask(__name__)
 
 
 # Flask Routes
-
 @app.route("/")
 def welcome():
     """Welcome to the Hawaii Climate Analyis Page! Navigate to any of the following routes."""
@@ -43,21 +43,48 @@ def welcome():
         f"/api/v1.0/<start><br/>"
         f"/api/v1.0/<start>/<end><br/>"
         f"===========================<br/>"
-        f"Note: Dates are YYYY-MM-DD format."
+        f"Note: Dates are YYYY-MM-DD format. Example:/api/v1.0/2017-11-29/2017-12-14"
     )
 
+# Get date that was a year prior to last data point. Query to obtain filtered data, using the date and prcp filters. Leave out days that don't have prcp data.
 @app.route("/api/v1.0/precipitation")
-def prcp():
+def precipitation():
+    dateYearAgo = dt.date(2017,8,23) - dt.timedelta(days=365)
+    prcp_data_query = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= dateYearAgo, Measurement.prcp != None).all()
+    prcp_values = []
+    for p in prcp_data_query:
+        precipitation_dic = {p.date: p.prcp}
+        prcp_values.append(precipitation_dic)
 
+    return jsonify(prcp_values)
+
+# Query to obtain all station data. use np.ravel to return the data into full list
 @app.route("/api/v1.0/stations")
 def stations():
+    station_query = session.query(Measurement.station).all()
+    all_stations = list(np.ravel(station_query))
+    return jsonify(all_stations)
 
+# Get date that was a year prior to last data point. Query to obtain tobs data. use np.ravel to return the data into full list
 @app.route("/api/v1.0/tobs")
 def tobs():
+    dateYearAgo = dt.date(2017,8,23) - dt.timedelta(days=365)
+    station_with_highest_tobs = session.query(Measurement.tobs).filter(Measurement.station == 'USC00519281', Measurement.date >= dateYearAgo).order_by(Measurement.tobs).all()
+    all_tobs = list(np.ravel(station_with_highest_tobs))
+    return jsonify(all_tobs)
+
 
 @app.route("/api/v1.0/<start>")
+def tobs_start(start):
+    tobs_start_query = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= start).all()
+    tobs_start_results = list(np.ravel(tobs_start_query))
+    return jsonify(tobs_start_results)
+
 @app.route("/api/v1.0/<start>/<end>")
-def info():
+def tobs_startend(start, end):
+    tobs_startend_query = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= start, Measurement.date <= end).all()
+    tobs_startend_results = list(np.ravel(tobs_startend_query))
+    return jsonify(tobs_startend_results)
 
 
 if __name__ == '__main__':
